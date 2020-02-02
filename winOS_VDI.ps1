b9a30aa1260cb0cc41375c8ecaf26416cdd0269d
tylerobara <49762914+tylerobara@users.noreply.github.com>
2020-02-02 11:37:42 -0500

    Update winOS_VDI.ps1
    added horizon downloader

--- winOS_VDI.ps1
+++ winOS_VDI.ps1
@@ -1,17 +1,34 @@
-$vdi_temp="$env:USERPROFILE/Documents/vdi_temp"
-$userdocs="$env:USERPROFILE/Documents/"
-New-Item -Path $userdocs -Name "vdi_temp" -ItemType "directory"
+<#
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+# Now running elevated so launch the script:
+& Write-Host "test"
+#>
 
+#"$env:USERPROFILE/Documents/GitHub/VDIscripts/winOS_VDI.ps1"
 
 Get-ChildItem $userdocs -Filter "vdi*"
 
+$vdi_temp="$env:USERPROFILE/Documents/vdi_temp"
+$userdocs="$env:USERPROFILE/Documents/"
+$vdi_test=Test-Path $vdi_temp
 
+If ($vdi_test -ne "True") {
+    New-Item -Path $userdocs -Name "vdi_temp" -ItemType "directory"
+} Else {
+    Write-Host "-----Temp directory already exists-----"
+}
+
+Get-ChildItem $vdi_temp
+
+<#
 Invoke-WebRequest -Uri "https://github.com/nsacyber/Windows-Secure-Host-Baseline/archive/master.zip" -OutFile $vdi_temp/master.zip
 
 Expand-Archive -LiteralPath "$vdi_temp/master.zip" -DestinationPath $vdi_temp
-
-
-Get-ChildItem $vdi_temp
 
 $RootcertificatesPath = "$vdi_temp\Windows-Secure-Host-Baseline-master\Certificates\Root"
 
@@ -28,5 +45,11 @@ $certificateFiles = @(Get-ChildItem -Path $IntercertificatesPath -Recurse -Inclu
 $certificateFiles | ForEach {
     Import-Certificate -FilePath $_.FullName -CertStoreLocation cert:\LocalMachine\CA # Intermediate Certification Authories
 }
+#>
 
+Invoke-WebRequest -Uri "https://download3.vmware.com/software/view/viewclients/CART20FQ4/VMware-Horizon-Client-5.3.0-15208953.exe" -OutFile $vdi_temp/horizon.exe
+Get-ChildItem $vdi_temp
+
+
+Write-Host "-----Cleaning up files and folders-----"
 #Remove-Item -path $vdi_temp -recurse
