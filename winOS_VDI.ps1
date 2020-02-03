f2dc4d8355b422d57bb7f31a2063720719e1bab0
tylerobara <49762914+tylerobara@users.noreply.github.com>
2020-02-02 20:13:20 -0500

    Update winOS_VDI.ps1
    fixed it

--- winOS_VDI.ps1
+++ winOS_VDI.ps1
@@ -1,10 +1,5 @@
 Write-Host "`n-----This script will down and install the certificates and software necessary to connect to the AFNET VDI desktops-----`n"
-$begin=Read-Host "`n-----Do you wish to continue? yes or no-----`n"
-
-Write-Host $begin
-
- If ($begin -eq "yes" -OR "y") {
-   
+  
 $vdi_temp="$env:USERPROFILE/Documents/vdi_temp"
 $userdocs="$env:USERPROFILE/Documents/"
 $vdi_test=Test-Path $vdi_temp
@@ -19,7 +14,7 @@ Invoke-WebRequest -Uri "http://militarycac.com/CACDrivers/DoDRoot2-5.p7b" -OutFi
 Import-Certificate -FilePath $vdi_temp/DoDRoot2-5.p7b -CertStoreLocation cert:\LocalMachine\Root # Trusted Root Certification Authories
 
 Invoke-WebRequest -Uri "http://militarycac.com/maccerts/AllCerts.p7b" -OutFile $vdi_temp/AllCerts.p7b
-Import-Certificate -FilePath $_.FullName -CertStoreLocation cert:\LocalMachine\CA # Intermediate Certification Authories
+Import-Certificate -FilePath $vdi_temp/AllCerts.p7b -CertStoreLocation cert:\LocalMachine\CA # Intermediate Certification Authories
 
 $vmview='C:\Program Files (x86)\VMware\VMware Horizon View Client\vmware-view.exe'
 $horizon_test=Test-Path $vmview
@@ -31,11 +26,8 @@ Invoke-WebRequest -Uri "https://download3.vmware.com/software/view/viewclients/C
 }
 
 Write-Host "`n-----Cleaning up files and folders-----`n"
-Remove-Item -path $vdi_temp -recurse
+#Remove-Item -path $vdi_temp -recurse
 
 Start-Process -FilePath $vmview -ArgumentList "--serverURL https://afrcdesktops.us.af.mil"
 
-} Else {
- Write-Host "Are you sure?"
- exit
- }
+exit
