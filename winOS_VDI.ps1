cec35b9a8c41e199ec63fe9da4e9b5c9d44495db
tylerobara <49762914+tylerobara@users.noreply.github.com>
2020-02-02 20:07:34 -0500

    Update winOS_VDI.ps1
    lets try this

--- winOS_VDI.ps1
+++ winOS_VDI.ps1
@@ -5,8 +5,6 @@ Write-Host $begin
 
  If ($begin -eq "yes" -OR "y") {
    
-#Get-ChildItem $userdocs -Filter "vdi*"
-
 $vdi_temp="$env:USERPROFILE/Documents/vdi_temp"
 $userdocs="$env:USERPROFILE/Documents/"
 $vdi_test=Test-Path $vdi_temp
@@ -17,27 +15,14 @@ If ($vdi_test -ne "True") {
     Write-Host "`n-----Temp directory already exists-----`n"
 }
 
-Invoke-WebRequest -Uri "https://github.com/nsacyber/Windows-Secure-Host-Baseline/archive/master.zip" -OutFile $vdi_temp/master.zip
+Invoke-WebRequest -Uri "http://militarycac.com/CACDrivers/DoDRoot2-5.p7b" -OutFile $vdi_temp/DoDRoot2-5.p7b
+Import-Certificate -FilePath $vdi_temp/DoDRoot2-5.p7b -CertStoreLocation cert:\LocalMachine\Root # Trusted Root Certification Authories
 
-Expand-Archive -LiteralPath "$vdi_temp/master.zip" -DestinationPath $vdi_temp
+Invoke-WebRequest -Uri "http://militarycac.com/maccerts/AllCerts.p7b" -OutFile $vdi_temp/AllCerts.p7b
+Import-Certificate -FilePath $_.FullName -CertStoreLocation cert:\LocalMachine\CA # Intermediate Certification Authories
 
-$RootcertificatesPath = "$vdi_temp\Windows-Secure-Host-Baseline-master\Certificates\Root"
-
-$certificateFiles = @(Get-ChildItem -Path $RootcertificatesPath -Recurse -Include *.cer | Where-Object { $_.PsIsContainer -eq $false})
-
-$certificateFiles | ForEach {
-    Import-Certificate -FilePath $_.FullName -CertStoreLocation cert:\LocalMachine\Root # Trusted Root Certification Authories
-}
-
-$IntercertificatesPath = "$vdi_temp\Windows-Secure-Host-Baseline-master\Certificates\Intermediate"
-
-$certificateFiles = @(Get-ChildItem -Path $IntercertificatesPath -Recurse -Include *.cer | Where-Object { $_.PsIsContainer -eq $false})
-
-$certificateFiles | ForEach {
-    Import-Certificate -FilePath $_.FullName -CertStoreLocation cert:\LocalMachine\CA # Intermediate Certification Authories
-}
-
-$horizon_test=Test-Path $vdi_temp/horizon.exe
+$vmview='C:\Program Files (x86)\VMware\VMware Horizon View Client\vmware-view.exe'
+$horizon_test=Test-Path $vmview
 
 If ($horizon_test -ne "True") {
 Invoke-WebRequest -Uri "https://download3.vmware.com/software/view/viewclients/CART20FQ4/VMware-Horizon-Client-5.3.0-15208953.exe" -OutFile $vdi_temp/horizon.exe
@@ -45,20 +30,12 @@ Invoke-WebRequest -Uri "https://download3.vmware.com/software/view/viewclients/C
     Write-Host "`n-----VMware Horizon Client installer already exists-----`n"
 }
 
-#Get-ChildItem $vdi_temp
-
-#Start-Process -FilePath $vdi_temp/horizon.exe -ArgumentList "/silent /norestart" -Verb RunAs
-
 Write-Host "`n-----Cleaning up files and folders-----`n"
-#Remove-Item -path $vdi_temp -recurse
+Remove-Item -path $vdi_temp -recurse
 
-$vmview='C:\Program Files (x86)\VMware\VMware Horizon View Client\vmware-view.exe'
-$vm_test=Test-Path $vmview
-If ($vm_test -eq "True") {
- Start-Process -FilePath $vmview -ArgumentList "-serverURL https://afrcdesktops.us.af.mil"
-} Else {
-Write-Host "Process failed, please start over"
-}
+Start-Process -FilePath $vmview -ArgumentList "--serverURL https://afrcdesktops.us.af.mil"
+
 } Else {
  Write-Host "Are you sure?"
+ exit
  }
