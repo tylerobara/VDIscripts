219e27989d1d9b32d97142f29a431c000f9d402c
tylerobara <49762914+tylerobara@users.noreply.github.com>
2022-03-24 08:28:47 -0400

    Update macOS_VDI.sh

--- macOS_VDI.sh
+++ macOS_VDI.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Set variables for VMware Horizon download url & temp directory for certs
-HORIZONURL="https://download3.vmware.com/software/view/viewclients/CART22FH2/VMware-Horizon-Client-2111-8.4.0-18968281.dmg"
+HORIZONURL="https://download3.vmware.com/software/CART23FQ1_MAC_2111_1/VMware-Horizon-Client-2111.1-8.4.1-19484634.dmg"
 CERTSDIR=${HOME}/certsdir
 
 #Deleting any cert remnants, downloading and installing certs, deleting un-needed certs directory
@@ -13,8 +13,7 @@ openssl pkcs7 -inform DER -outform PEM -in AllCerts.p7b -print_certs > AllCerts.
 sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain AllCerts.cer
 RootCerts=( RootCert2 RootCert3 RootCert4 RootCert5 )
 for cert in "${RootCerts[@]}"
-    do
-        :
+    do:
         curl -s https://militarycac.com/maccerts/$cert".cer" -o $cert".cer"
         sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $cert".cer"
 done
